8f69afb9442cc716adc3eec8feb16a29aff21c94
root <root@srv6502750376.bitcommand.net>
2025-10-24 20:01:56 +0000

    .

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "Post2",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Post2.0.js",          // your Node entry
+      env: {
+        NODE_ENV: "production",
+        // Make child_process 'python' resolve to the venv interpreter
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+      }
+    }
+  ]
+}

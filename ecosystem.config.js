de1327f64c6a4abc969a3cc05540f2625e4c738a
alihmian <alihmiann@gmail.com>
2025-10-25 08:44:08 +0330

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,144 @@
 module.exports = {
   apps: [
     {
-      name: "Post2",
+      name: "Post",
       cwd: "/root/CaptionCrafter2.0",
-      script: "build/Post2.0.js",          // your Node entry
+      script: "build/Post.js",
       env: {
         NODE_ENV: "production",
-        // Make child_process 'python' resolve to the venv interpreter
         PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
-        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Post2",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Post2.0.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Currency",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Currency.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Post_alt",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/bot.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
       }
     },
     {
       name: "BN",
       cwd: "/root/CaptionCrafter2.0",
-      script: "build/BreakingNews.js",          // your Node entry
+      script: "build/BreakingNews.js",
       env: {
         NODE_ENV: "production",
-        // Make child_process 'python' resolve to the venv interpreter
         PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
-        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Gold",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Gold.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Crypto",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Crypto.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Screenshot",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Screenshot.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Car1",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Car1.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "Car2",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Car2.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "buuut",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "dist/buuut.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
       }
     },
     {
       name: "Live",
       cwd: "/root/CaptionCrafter2.0",
-      script: "build/Live.js",          // your Node entry
+      script: "build/Live.js",
       env: {
         NODE_ENV: "production",
-        // Make child_process 'python' resolve to the venv interpreter
         PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
-        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "iPhone",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/iPhone.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
+      }
+    },
+    {
+      name: "samsung",
+      cwd: "/root/CaptionCrafter2.0",
+      script: "build/Samsung.js",
+      env: {
+        NODE_ENV: "production",
+        PYTHON: "/root/CaptionCrafter2.0/venv/bin/python",
+        PATH: "/root/CaptionCrafter2.0/venv/bin:/usr/local/bin:/usr/bin:/bin"
       }
     }
   ]
-}
+};
